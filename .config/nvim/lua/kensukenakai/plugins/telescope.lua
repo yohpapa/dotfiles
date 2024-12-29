14c9224f418ee739f37a6a0a719d2c8334828a4e
Kensune Nakai <kensukenakai@yohpapa.com>
2024-12-29 23:30:43 +0900

    (nvim) added telescope configuration

---
+++ .config/nvim/lua/kensukenakai/plugins/telescope.lua
@@ -0,0 +1,28 @@
+return {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        local telescope = require("telescope")
+        local builtin = require('telescope.builtin')
+
+        telescope.load_extension("fzf")
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+        -- telescope.setup({
+        --     pickers = {
+        --         find_files = { theme = "ivy"},
+        --         live_grep = { theme = "ivy"},
+        --         buffers = { theme = "ivy" },
+        --         help_tags = { theme = "ivy" },
+        --     },
+        -- })
+    end,
+}
