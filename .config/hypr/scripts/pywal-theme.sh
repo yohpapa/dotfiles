cf1c155f8cc1a7f72eb48a7a4cc00a1e5027046e
Kensuke Nakai <kemumaki.kemuo@gmail.com>
2025-09-22 16:24:53 +0900

    (mako) change style. (hypr) auto-start tmux on ghostty and improve wallpaper change ux.

--- .config/hypr/scripts/pywal-theme.sh
+++ .config/hypr/scripts/pywal-theme.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+#                            _       _   _                               _
+#  _ __  _   ___      ____ _| |     | |_| |__   ___ _ __ ___   ___   ___| |__
+# | '_ \| | | \ \ /\ / / _` | |_____| __| '_ \ / _ \ '_ ` _ \ / _ \ / __| '_ \
+# | |_) | |_| |\ V  V / (_| | |_____| |_| | | |  __/ | | | | |  __/_\__ \ | | |
+# | .__/ \__, | \_/\_/ \__,_|_|      \__|_| |_|\___|_| |_| |_|\___(_)___/_| |_|
+# |_|    |___/
+
+source "${HOME}/.config/hypr/scripts/mako-config.sh"
+
 walpaper_directory=$HOME/pictures/wallpapers
 wallpaper_path=$(find "$walpaper_directory" -type f -print0 | shuf -z -n 1)
 
@@ -8,11 +17,8 @@ if [[ -z "$wallpaper_path" ]]; then
   exit 1
 fi
 
-# Run pywal to generate the theme and save the wallpaper path
 wal -i "$wallpaper_path"
-
-# Set the wallpaper using swww
 swww img "$wallpaper_path" --transition-type outer --transition-fps 30 --transition-duration 2
+update_mako_config
 
-# Optional: reload Waybar and other applications that depend on pywal's theme
-# killall -SIGUSR1 waybar
+notify-send -i "$wallpaper_path" "The wallpaper has changed!"
