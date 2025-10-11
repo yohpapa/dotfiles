d81055a23b3166ce71ba022a47553bd29d8cbf19
Kensuke Nakai <kemumaki.kemuo@gmail.com>
2025-10-11 21:45:29 +0900

    (waybar) modularize its config for niri and hyprland.

---
+++ .config/scripts/waybar.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+#                      _                    _
+# __      ____ _ _   _| |__   __ _ _ __ ___| |__
+# \ \ /\ / / _` | | | | '_ \ / _` | '__/ __| '_ \
+#  \ V  V / (_| | |_| | |_) | (_| | | _\__ \ | | |
+#   \_/\_/ \__,_|\__, |_.__/ \__,_|_|(_)___/_| |_|
+#                |___/
+
+compositor=$(~/.config/scripts/check_compositor.sh)
+config_dir="$HOME/.config/waybar"
+source_config=""
+
+if [ "$compositor" = "hyprland" ]; then
+  source_config="$config_dir/config-hyprland.jsonc"
+elif [ "$compositor" = "niri" ]; then
+  source_config="$config_dir/config-niri.jsonc"
+fi
+
+if [ ! -f "$source_config" ]; then
+    echo "Error: Config file not found: $source_config"
+    exit 1
+fi
+
+ln -sf "$source_config" "$config_dir/config.jsonc"
+
+waybar &
