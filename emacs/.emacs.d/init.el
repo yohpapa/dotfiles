bee09687f9c537a492c8f69944f6d06d73fff79b
Kensuke Nakai <kemumaki.kemuo@gmail.com>
2025-10-19 16:15:31 +0900

    (emacs) ui tweaks and suport vertico.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,8 +1,26 @@
-;;  _       _ _         _
+
 ;; (_)_ __ (_) |_   ___| |
 ;; | | '_ \| | __| / _ \ |
 ;; | | | | | | |_ |  __/ |
 ;; |_|_| |_|_|\__(_)___|_|
+
+;; bootstrap for straight-use-package.el
+;; https://github.com/radian-software/straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
 ;;; package repositories
 (setq package-archives 
@@ -56,5 +74,29 @@
 (add-to-list 'default-frame-alist
 	     '(font . "FiraCode Nerd Font Mono-7"))
 
+;; fixed emacs window size upon startup
+(setq frame-inhibit-implied-resize t)
+
 ;; common settings
 (setq auto-save-default nil)
+
+;; vertico
+(use-package vertico
+  :ensure t
+  :bind (:map vertico-map
+	      ("C-j" . vertico-next)
+	      ("C-k" . vertico-previous)
+	      ("C-f" . vertico-exit)
+	      :map minibuffer-local-map
+	      ("C-h" . backward-kill-word))
+  :custom
+  (vertico-cycle t)
+  :init
+  (vertico-mode))
+
+(use-package savehist
+  :init
+  (savehist-mode))
+
+(straight-use-package '(marginalia :host github :repo "minad/marginalia" :branch "main"))
+(marginalia-mode)
